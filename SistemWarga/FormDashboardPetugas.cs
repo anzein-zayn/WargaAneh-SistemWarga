0fbb4f6090c7cf81f705b99211a0b8267d244fb9
anzein-zayn <andy.zaky.ft24@mail.umy.ac.id>
2026-04-19 21:27:49 +0700

    Membuat file FormDashboardPetugas.cs

---
+++ SistemWarga/FormDashboardPetugas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemWarga
+{
+    public partial class FormDashboardPetugas: Form
+    {
+        public FormDashboardPetugas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWarga_Click(object sender, EventArgs e)
+        {
+            FormWargaPetugas adminWargaP = new FormWargaPetugas();
+            adminWargaP.Show();
+        }
+        private void btnKartuKel_Click(object sender, EventArgs e)
+        {
+            FormKeluargaPetugas adminWargaP = new FormKeluargaPetugas();
+            adminWargaP.Show();
+        }
+        private void btnSurat_Click(object sender, EventArgs e)
+        {
+            FormSuratPetugas adminWargaP = new FormSuratPetugas();
+            adminWargaP.Show();
+        }
+        
+    }
+}
